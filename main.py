2d0375159c352e7e9570ec814fedf69a8c07e901
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2019-03-14 10:55:23 +0700

    MODULARISASI TAHAP-1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print('Hello World!')
 
 alas = 10
 tinggi = 30
-luas_segitiga = alas * tinggi
+luas_segitiga = alas * tinggi / 2
 print(luas_segitiga)
 
 # Menggunakan logika percabangan
@@ -34,3 +34,25 @@ print('10', luas_segitiga)
 print('Dengan for')
 for i in range(0, 10):
     print(i+1, luas_segitiga)
+
+
+# MODULARISASI TAHAP 1: PEMBUATAN FUNGSI
+print('Segitiga 1')
+alas = 5
+tinggi = 15
+luas_segitiga = alas * tinggi / 2
+print(luas_segitiga)
+
+print('Segitiga 2')
+alas = 3
+tinggi = 6
+luas_segitiga = alas * tinggi / 2
+print(luas_segitiga)
+
+
+def hitung_luas_segitiga(title, alas, tinggi):
+    print(title)
+    return alas * tinggi / 2
+
+print(hitung_luas_segitiga('Segitiga 3', 5, 15))
+print(hitung_luas_segitiga('Segitiga 4', 3, 6))
